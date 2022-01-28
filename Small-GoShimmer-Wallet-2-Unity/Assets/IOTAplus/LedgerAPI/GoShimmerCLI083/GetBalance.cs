86e08e9697908e7126da0788f4eebd0a84ff9e34
Edward Falzon <falzon@pm.me>
2022-01-28 22:43:30 +0800

    Refactoring and code clean-up. No new functionality.

--- Small-GoShimmer-Wallet-2-Unity/Assets/IOTAplus/LedgerAPI/GoShimmerCLI083/GetBalance.cs
+++ Small-GoShimmer-Wallet-2-Unity/Assets/IOTAplus/LedgerAPI/GoShimmerCLI083/GetBalance.cs
@@ -1,11 +1,8 @@
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Text.RegularExpressions;
-using UnityEngine;
-using IOTAplus.Utilities;
 using IOTAplus.LedgerAPI.Wallet;
 
 
@@ -14,21 +11,20 @@ namespace IOTAplus.LedgerAPI.GoShimmerCLI083
 	public class GetBalance
 	{
 		// The processParameter is the text passed to the CLI as a single string, containing one or more parameters.
-		private const string processParameter = "balance";
+		private const string PROCESS_PARAMETER = "balance";
 
 		// Because we're parsing text output from the CLI, we need to specify some markers, to parse the text correctly.
 		
-		// Markers for GetBalance:
 		// Lines beginning with these strings are not important to the parser.
-		readonly string[] throwAwayLines = { "-----", "FETCH", "IOTA " };
+		private readonly string[] _throwAwayLines = { "-----", "FETCH", "IOTA " };
 		
 		// We watch for headings of the two returned tables of coin-balances and owned NFTs.
-		const string balancesHeader = "AVAIL";
-		const string nftHeader      = "OWNED";
-		const string fieldNames     = "STATU";
+		private const string BALANCES_HEADER = "AVAIL";
+		private const string NFT_HEADER      = "OWNED";
+		private const string FIELD_NAMES     = "STATU";
 
-		protected GoShimmerCLI083 _api;
-		protected IWallet         _wallet;
+		private readonly GoShimmerCLI083 _api;
+		private readonly IWallet         _wallet;
 
 		internal GetBalance (GoShimmerCLI083 api, IWallet wallet)
 		{
@@ -36,13 +32,12 @@ namespace IOTAplus.LedgerAPI.GoShimmerCLI083
 			_wallet = wallet;
 		}
 
-		public IEnumerator Execute (Action OnSuccess = null, Action OnFailure = null)
+		public IEnumerator Execute (Action onSuccess = null, Action onFailure = null)
 		{
-			Debug.Log ("Executing GetBalance.");
-			if (_api != null) yield return _api.StartCoroutine (_api.StartProcess (processParameter, ParseBalanceOutput));
+			if (_api != null) yield return _api.StartCoroutine (_api.StartProcess (PROCESS_PARAMETER, ParseBalanceOutput));
 			
 			// TODO: Check for success condition.
-			OnSuccess?.Invoke ();
+			onSuccess?.Invoke ();
 		}
 		
 		private void ParseBalanceOutput (string balanceOutput)
@@ -69,10 +64,6 @@ namespace IOTAplus.LedgerAPI.GoShimmerCLI083
       [ OK ]  oWXV7Ufgyh3dZayW5ozQcdqEN5s4oLwqrPrm8NUwxXVR  100 I              IOTA                                            IOTA
 */
 
-			string logString = string.Concat (this.GetType (), ".", MethodInfo.GetCurrentMethod ().Name, ":\n");
-
-			bool inNftLines = false;
-			
 			// The parser works with two lists in succession. The active list is referenced by workingList.
 			var workingList = new List<Dictionary<string, string>> ();
 
@@ -86,17 +77,15 @@ namespace IOTAplus.LedgerAPI.GoShimmerCLI083
 				var lineStart = line.Substring (0, 5).ToUpper();
 				
 				// Skip lines the parser doesn't need.
-				if (throwAwayLines.Contains (lineStart)) continue;
+				if (_throwAwayLines.Contains (lineStart)) continue;
 
 				switch (lineStart)
 				{
-					case balancesHeader:
-						inNftLines  = false;
+					case BALANCES_HEADER:
 						workingList = _wallet.CoinBalances;
 						workingList.Clear ();
 						continue;
-					case nftHeader:
-						inNftLines  = true;
+					case NFT_HEADER:
 						workingList = _wallet.OwnedNFTs;
 						workingList.Clear ();
 						continue;
@@ -113,51 +102,6 @@ namespace IOTAplus.LedgerAPI.GoShimmerCLI083
 				if (CheckForHeadings (lineStart, headings, words)) continue;
 
 				ParseBalanceEntry (words, headings, workingList);
-
-				if (inNftLines && line.StartsWith ("\t"))
-				{
-					words = Regex.Split (line, @"\t").Where (s => s != String.Empty).ToArray<string> ();
-
-					//Console.WriteLine("balanceOutput " + words[0] + " color " + words[1] + " name " + words[2]);
-					//print("Amount of object in list: "+(gameobjectList.Count - 1).ToString());
-
-					// for (int i = 0 ; i < (gameobjectList.Count-1); i++) {
-					//   print(i.ToString());
-					// try
-					//{
-					//  if (gameobjectList[i].gameObject.GetComponent<AssetGameObject>().withdrawAssetinNFT == false)
-					// {
-					//    index = i;
-					//    print("Index found maching == false: "+index.ToString());
-					//}
-					//}
-					//catch { };
-
-					//}
-
-					//						var newObject2 = gameobjectList[gameobjectList.Count - 1].GetComponent<AssetGameObject> ()
-					//								.CreateNewAssetInNFTGameobject (); //Instantiate(nfttGameobject, assetGameobject.transform.parent);
-
-					//print("Number of objects in generated object list: " + (gameobjectList.Count - 1).ToString());
-
-					//var newObject2 = Instantiate(assetInNfttGameobject, newObject.transform.parent);
-					//newObject2.SetActive(true);
-
-					//newObject2.GetComponent<AssetGameObject>().AssetInNFT.SetActive(true);
-
-					//						string nftid = gameobjectList[gameobjectList.Count - 1].GetComponent<AssetGameObject> ().nftId.text;
-
-					//gameobjectList.Add(newObject);
-					//						newObject2.gameObject.GetComponent<AssetGameObject> ().isNFT              = true;
-					//						newObject2.gameObject.GetComponent<AssetGameObject> ().withdrawAssetinNFT = true;
-					//						newObject2.gameObject.GetComponent<AssetGameObject> ().nftIDString        = nftid;
-					//						newObject2.gameObject.GetComponent<AssetGameObject> ().color.text         = words[1];
-					//						newObject2.gameObject.GetComponent<AssetGameObject> ().balance.text       = words[0];
-					//						newObject2.gameObject.GetComponent<AssetGameObject> ().tokenName.text     = words[2];
-
-					//newObject.gameObject.GetComponent<AssetGameObject>().amountToSend.GetComponent<InputField>().placeholder.GetComponent<Text>().text = "all";
-					//newObject.gameObject.GetComponent<AssetGameObject>().amountToSend.GetComponent<InputField>().readOnly = true;
-				}
 			}
 		}
 
@@ -176,14 +120,14 @@ namespace IOTAplus.LedgerAPI.GoShimmerCLI083
 			// Add a new, blank entry to the end of workingList.
 			workingList.Add (new Dictionary<string, string> ());
 
-			// The line data will match the order of the headings, which we use as the keys in the dictionary. 
+			// The line data will match the order of the headings, which we use as the keys in the dictionary.
 			for (int i = 0; i < words.Length; i++) workingList.Last ().Add (headings[i], words[i]);
 		}
 
 		private bool CheckForHeadings (string lineStart, List<string> headings, string[] words)
 		{
 			// Extract the heading line of the table, to use as field names in the dictionary.
-			if (string.Equals (lineStart, fieldNames))
+			if (string.Equals (lineStart, FIELD_NAMES))
 			{
 				headings.Clear ();
 				foreach (var s in words) headings.Add (s);
--- Small-GoShimmer-Wallet-2-Unity/Assets/IOTAplus/LedgerAPI/GoShimmerCLI083/GoShimmerCLI083.cs
+++ Small-GoShimmer-Wallet-2-Unity/Assets/IOTAplus/LedgerAPI/GoShimmerCLI083/GoShimmerCLI083.cs
@@ -1,11 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
-using System.Text.RegularExpressions;
-using IOTAplus.Utilities;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 using IOTAplus.LedgerAPI.Wallet;
@@ -30,27 +25,16 @@ namespace IOTAplus.LedgerAPI.GoShimmerCLI083
 	public class GoShimmerCLI083 : LedgerAPIBase
 	{
 		private IWallet _wallet;
-		// Because we're parsing text output from the CLI, we need to specify some markers, to parse the text correctly.
-		
-		// Markers for GetBalance:
-		// Lines beginning with these strings are not important to the parser.
-		readonly string[] throwAwayLines = new String[] { "-----", "FETCH", "IOTA " };
-		
-		// We watch for headings of the two returned tables of coin-balances and owned NFTs.
-		const string balancesHeader = "AVAIL";
-		const string nftHeader      = "OWNED";
-		const string fieldNames     = "STATU";
-
 		
 #if UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
-			private string path = Application.streamingAssetsPath + "/cli-wallet-mac";
+		private string path = Application.streamingAssetsPath + "/cli-wallet-mac";
 #elif UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
-			private string path = Application.streamingAssetsPath + "/cli-wallet-win.exe";
+		private string path = Application.streamingAssetsPath + "/cli-wallet-win.exe";
 #elif UNITY_STANDALONE_LINUX || UNITY_EDITOR_LINUX
 		private string path = Application.streamingAssetsPath + "/cli-wallet-linux";
 #else
-			private string path = "CLI not supported on this platform.";
-			Debug.LogError (path);
+		private string path = "CLI not supported on this platform.";
+		Debug.LogError (path);
 #endif
 
 
@@ -62,35 +46,26 @@ namespace IOTAplus.LedgerAPI.GoShimmerCLI083
 				throw new System.ApplicationException("ERROR! No Wallet component found.");
 		}
 
+		
 		public override void GetBalance ()
 		{
-			Debug.Log ("Calling GetBalance.");
 			LedgerAPI.GoShimmerCLI083.GetBalance command = new GetBalance (this, _wallet);
 			StartCoroutine (command.Execute (_OnGetBalanceSuccess, _OnGetBalanceFailure));
 		}
 		
-		protected INft ParseNFT (string line)
-		{
-			return null;
-		}
-				
-				
+		
 		/// <summary>
 		/// Calls the GoShimmer wallet CLI, passing in the provided parameters.
 		/// </summary>
 		/// <param name="parameters">Parameters to pass to the CLI</param>
 		internal IEnumerator StartProcess (string parameters, Action<string> callback = null)
 		{
-			string logString = string.Concat (this.GetType (), ".", MethodInfo.GetCurrentMethod ().Name, ":\n");
+			string logString = string.Concat (this.GetType (), ".", nameof(this.StartProcess), ":\n");
 
 			if (string.IsNullOrEmpty (parameters))
 			{
 				Debug.LogError (string.Concat(logString, " -- ERROR -- No parameters provided."));
 			}
-
-#if DEBUG
-			Debug.Log (String.Concat (logString, parameters));
-#endif
 
 			Process process = new Process ();
 			process.StartInfo.FileName  = path;
@@ -106,6 +81,7 @@ namespace IOTAplus.LedgerAPI.GoShimmerCLI083
 #if DEBUG
 			float startTime = Time.realtimeSinceStartup;
 #endif
+			
 			while (!process.HasExited) yield return null;
 
 #if DEBUG
